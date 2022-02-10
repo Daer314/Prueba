b010fcdc0df600c2a5d2ff831c3560de0741d76f
Diego <daer314@hotmail.com>
2022-02-10 14:04:48 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [players, setPlayers] = useState([])
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+  const fetchData = async () => {
+    const data = await fetch('https://mach-eight.uc.r.appspot.com/')
+    const response = await data.json()
+    setPlayers(response.values)
+  }
+
+  const getData = (number) => {
+    setHeight(number.target.value)
+  }
+
+  const filteredPlayers = height ? 
+    players.filter(player => player.h_in > height)
+           .map((player,index) => {
+             return (
+               <li key={index}>{`${player.first_name} ${player.last_name}`}</li>
+             )
+           }) 
+    : [];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>NBA Players</h1>
+      <p>Please enter the height</p>
+      <input type='number' onChange={getData} placeholder='search'/>
+      <ul>
+       { 
+        !!filteredPlayers.length ? 
+        (<ul data-testid='players-name'>
+          {filteredPlayers}
+        </ul>) 
+        : <p>No matches found</p>
+       }
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
+import App from './App.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App tests', () => {
+
+    it('renders h1', async () => {
+        render(<App />);
+        const headingElement = screen.getByText(/NBA Players/i);
+        expect(headingElement).toBeInTheDocument();
+    })
+
+    it('Render instruction Please enter the height', async () => {
+        render(<App />);
+        const instructionElement = screen.getByText(/Please enter the height/i);
+        expect(instructionElement).toBeInTheDocument();
+    })
+
+    it('Should render input', async () => {
+        render(<App />);
+        const inputElement = screen.getByPlaceholderText(/search/i);
+        expect(inputElement).toBeInTheDocument();
+    })
+
+    it('Should be able to type in input', async () => {
+        render(<App />);
+        const inputElement = screen.getByPlaceholderText(/search/i);
+        fireEvent.change(inputElement, { target: { value: '80'}})
+        expect(inputElement.value).toBe('80');
+    })
+
+    it('Should display No matches found when the input is empty', async () => {
+        render(<App />);
+        const pElement = await screen.findByText(/No matches found/i)
+        expect(pElement).toBeInTheDocument();
+    })
+
+    it('Should display No matches found when does not match player height', async () => {
+        render(<App />);
+        const inputElement = screen.getByPlaceholderText(/search/i);
+        fireEvent.change(inputElement, { target: { value: 95 }})
+        const pElement = await screen.findByText(/No matches found/i)
+        expect(pElement).toBeInTheDocument();
+    })
+
+    it('Return element if I type a number that matches height in the input field', async () => {
+        render(<App />);
+        const inputElement = screen.getByPlaceholderText(/search/i);
+        fireEvent.change(inputElement, { target: { value: '1'}})
+        const listElement = await screen.findByTestId('players-name');
+        expect(listElement).toBeInTheDocument();
+    })
+
+})
